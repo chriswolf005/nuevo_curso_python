68fb003cb4d430d15c34d6e9a528b8290799b3e4
Christopher Sanchez <christophersanchezwolf005@gmail.com>
2024-11-08 19:58:01 -0400

    Bucles y control

---
+++ class 11.py
@@ -0,0 +1,35 @@
+'''x=5
+if x>5:
+    print("x es mayor a 5")
+elif x==5:
+    print("X es igual que 5")
+else:
+    print("X es mejor que 5")
+print(" fuera")
+
+
+x=15
+y=15
+
+if x>10 and y>25:
+    print("x es mayor que 10 y mayor que 15")
+
+if x>10 or y >25:
+     print("x es mayor que 10 o mayor que 15")
+
+if not x>10:
+     print("x no es mayor que 10")
+
+'''
+
+is_menber=True
+age=15
+
+if is_menber:
+    if age>=15:
+        print("Acceso")
+    else:
+        print("Acceso denegado eres menor de 15")
+else:
+    print("No eres miembro fuera!")
+
