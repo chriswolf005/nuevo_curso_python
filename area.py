39f22d1a06709e12f7a6f946a9749043b1419c8a
Christopher Sanchez <christophersanchezwolf005@gmail.com>
2025-01-02 20:57:51 -0400

    Primer ejercicio del libro y se agregaron ejemplos de comentarios

---
+++ area.py
@@ -0,0 +1,9 @@
+def calculate_area(base,height):
+    """
+    Calcula el area de un triangulo
+    """
+
+
+
+    #Multiplicamos la base por altura y luego lo dividimos entre dos
+    return (base*height)/2
